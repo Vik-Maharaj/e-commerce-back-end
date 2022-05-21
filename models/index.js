8b9161e51d97d2fee168a46936b07ae9bb89fff3
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-21 16:00:43 -0400

    Update model index js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,28 @@
-// import models
+// imports all models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+// exports models
+module.exports = { Product, Category, Tag, ProductTag };
