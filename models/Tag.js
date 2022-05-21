beb8093b1546ac70ddc8712c76baa975cba8b100
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-21 18:53:51 -0400

    Update tag model js

--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// Set Up columns, fields, and data types for DataCom Tag Model
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
